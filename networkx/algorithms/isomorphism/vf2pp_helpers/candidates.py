4f135b02d10af534920f9c06dc9e7a3023ac1586
kpetridis24 <petridkon@gmail.com>
2022-08-12 13:16:07 +0300

    Separate candidate selection method for SUB and ISO problems

--- networkx/algorithms/isomorphism/vf2pp_helpers/candidates.py
+++ networkx/algorithms/isomorphism/vf2pp_helpers/candidates.py
@@ -36,6 +36,13 @@ def _find_candidates(u, graph_params, state_params, PT="iso"):
     candidates: set
         The nodes from G2 which are candidates for u.
     """
+    if PT == "iso":
+        return _find_candidates_ISO(u, graph_params, state_params)
+    elif PT == "sub":
+        return _find_candidates_SUB(u, graph_params, state_params)
+
+
+def _find_candidates_ISO(u, graph_params, state_params):
     (
         G1,
         G2,
@@ -49,19 +56,14 @@ def _find_candidates(u, graph_params, state_params, PT="iso"):
 
     covered_neighbors = [nbr for nbr in G1[u] if nbr in mapping]
     if not covered_neighbors:
-        if PT == "iso":
-            return {
-                node
-                for node in G2.nodes()
-                if node not in reverse_mapping
-                and all(nbr2 not in reverse_mapping for nbr2 in G2[node])
-            }.intersection(
-                *[nodes_of_G2Labels[G1_labels[u]], G2_nodes_of_degree[G1.degree[u]]]
-            )
-        elif PT == "sub":
-            return {
-                node for node in G2.nodes() if node not in reverse_mapping
-            }.intersection(nodes_of_G2Labels[G1_labels[u]])
+        return {
+            node
+            for node in G2.nodes()
+            if node not in reverse_mapping
+            and all(nbr2 not in reverse_mapping for nbr2 in G2[node])
+        }.intersection(
+            *[nodes_of_G2Labels[G1_labels[u]], G2_nodes_of_degree[G1.degree[u]]]
+        )
 
     nbr1 = covered_neighbors[0]
     common_nodes = {nbr2 for nbr2 in G2[mapping[nbr1]]}
@@ -69,13 +71,33 @@ def _find_candidates(u, graph_params, state_params, PT="iso"):
     for nbr1 in covered_neighbors[1:]:
         common_nodes.intersection_update(G2[mapping[nbr1]])
 
-    if PT == "iso":
+    return {node for node in common_nodes if node not in reverse_mapping}.intersection(
+        *[nodes_of_G2Labels[G1_labels[u]], G2_nodes_of_degree[G1.degree[u]]]
+    )
+
+
+def _find_candidates_SUB(u, graph_params, state_params):
+    (
+        G1,
+        G2,
+        G1_labels,
+        G2_labels,
+        _,
+        nodes_of_G2Labels,
+        G2_nodes_of_degree,
+    ) = graph_params
+    mapping, reverse_mapping, _, _, _, _ = state_params
+
+    covered_neighbors = [nbr for nbr in G1[u] if nbr in mapping]
+    if not covered_neighbors:
         return {
-            node for node in common_nodes if node not in reverse_mapping
-        }.intersection(
-            *[nodes_of_G2Labels[G1_labels[u]], G2_nodes_of_degree[G1.degree[u]]]
-        )
-    elif PT == "sub":
-        return {node for node in common_nodes}.intersection(
-            nodes_of_G2Labels[G1_labels[u]]
-        )
+            node for node in G2.nodes() if node not in reverse_mapping
+        }.intersection(nodes_of_G2Labels[G1_labels[u]])
+
+    nbr1 = covered_neighbors[0]
+    common_nodes = {nbr2 for nbr2 in G2[mapping[nbr1]]}
+
+    for nbr1 in covered_neighbors[1:]:
+        common_nodes.intersection_update(G2[mapping[nbr1]])
+
+    return {node for node in common_nodes}.intersection(nodes_of_G2Labels[G1_labels[u]])
